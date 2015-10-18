a44c42e6eed4d04eedcdc6160a775ec2f0ab75db
Owner <Owner@MaxVAIO>
2015-10-18 14:12:23 -0500

    HelpFunc Update

--- AI_HW2_Java/src/wargame/HelpFunc.java
+++ AI_HW2_Java/src/wargame/HelpFunc.java
@@ -4,15 +4,13 @@ public static class HelpFunc {
 	public int[][] textToMatrix(String textFile){
 		int [][] board = new int [6][6];
 		
-		FileReader input = new FileReader("/HW2project/src/wargame/res" + textFile);
+		FileReader input = new FileReader("/HW2project/src/wargame/res/" + textFile);
 		BufferedReader bufRead = new BufferedReader(input);
 		String myLine = null;
 
-		while ( (myLine = bufRead.readLine()) != null)
+		for (int i = 0; (myLine = bufRead.readLine()) != null; i++)
 		{    
-		    String[] array1 = myLine.split(":");
-		    // check to make sure you have valid data
-		    String[] array2 = array1[1].split(" ");
+		    String[i] = myLine.split('TAB');
 		    for (int i = 0; i < array2.length; i++)
 		        function(array1[0], array2[i]);
 		}
